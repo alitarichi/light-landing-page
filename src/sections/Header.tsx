9dbd7b03b18952e4c2ca548ff7d971ed8cc36be7
ali tarichi <tarichiali@gmail.com>
2024-07-23 18:39:26 +0300

    header inital commit

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,14 +6,17 @@ import MenuIcon from "@/assets/menu.svg";
 export const Header = () => {
   return (
     <header className="sticky top-0">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+          Stramline your workflow and boost your productivity
+        </p>
         <div className="inline-flex gap-1 items-center">
           <p>Get started for FREE</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
         </div>
       </div>
       <div className="py-5">
-        <div className="container">
+        <div className="px-10">
           <div className="flex items-center justify-between">
             <Image src={Logo} alt="Saas Logo" height={40} width={40} />
             <MenuIcon className="h-5 w-5 md:hidden" />
