1c267a0916bb356351594a4f95adb594111a2c85
ali tarichi <tarichiali@gmail.com>
2024-07-24 14:25:23 +0300

    hero section images added and responive classes added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,32 +1,54 @@
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
 import Image from "next/image";
+import cylinderImage from "@/assets/cylinder.png";
+import noodle from "@/assets/noodle.png";
 
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
-        <div>
-          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
-            Version 2.0 is here
+        <div className="md:flex items-center">
+          <div className="md:w-[478px]">
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div>
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+              Pathway to productivity
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tighter mt-6">
+              Celebrate the joy of accomplishment with an app designed to track
+              your progress, motivate your effors and Celebrate your success.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button>
+            </div>
           </div>
-          <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
-            Pathway to productivity
-          </h1>
-          <p className="text-xl text-[#010D3E] tracking-tighter mt-6">
-            Celebrate the joy of accomplishment with an app designed to track
-            your progress, motivate your effors and Celebrate your success.
-          </p>
-          <div className="flex gap-1 items-center mt-[30px]">
-            <button className="btn btn-primary">Get for free</button>
-            <button className="btn btn-text gap-1">
-              <span>Learn more</span>
-              <ArrowIcon className="h-5 w-5" />
-            </button>
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src={cogImage}
+              alt="cog image"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+            />
+            <Image
+              src={cylinderImage}
+              width={220}
+              height={220}
+              alt="cylinder Image "
+              className="hidden md:block -top-8 -left-32 md:absolute"
+            />
+            <Image
+              src={noodle}
+              width={220}
+              height={220}
+              alt="noodle image"
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+            />
           </div>
-        </div>
-        <div className="mt-20">
-          <Image src={cogImage} alt="cog image" />
         </div>
       </div>
     </section>
