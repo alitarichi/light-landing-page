9492beae1d3cad4291f4a9f40de3a4e694133423
ali tarichi <tarichiali@gmail.com>
2024-07-29 18:14:25 +0300

    added css classes

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -3,14 +3,14 @@ export const CallToAction = () => {
     <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
       <div className="container">
         <h2 className="section-title">Sign up for free today</h2>
-        <p className="section-description">
+        <p className="section-description mt-5">
           Celebrate the joy of accomplishment with an app designed to track your
           progress and motivate your efforts .
         </p>
       </div>
-      <div>
-        <button>Get for free</button>
-        <button>Learn more</button>
+      <div className="flex gap-2 mt-10 justify-center">
+        <button className="btn btn-primary">Get for free</button>
+        <button className="btn btn-text">Learn more</button>
       </div>
     </section>
   );
