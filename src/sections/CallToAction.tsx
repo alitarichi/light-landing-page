38bbb7a221ea700c200ce5284bf7f499486fac2d
ali tarichi <tarichiali@gmail.com>
2024-07-29 18:22:55 +0300

    cta images added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,16 +1,38 @@
+import ArrowRight from "@/assets/arrow-right.svg";
+import starImage from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
+import Image from "next/image";
+
 export const CallToAction = () => {
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <h2 className="section-title">Sign up for free today</h2>
-        <p className="section-description mt-5">
-          Celebrate the joy of accomplishment with an app designed to track your
-          progress and motivate your efforts .
-        </p>
+        <div className="section-heading relative">
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts .
+          </p>
+          <Image
+            src={starImage}
+            alt="star image"
+            width={360}
+            className="absolute -left-[350px] -top-[137px]"
+          />
+          <Image
+            src={springImage}
+            alt="spring image"
+            width={360}
+            className="absolute -right-[331px] -top-[19px]"
+          />
+        </div>
       </div>
       <div className="flex gap-2 mt-10 justify-center">
         <button className="btn btn-primary">Get for free</button>
-        <button className="btn btn-text">Learn more</button>
+        <button className="btn btn-text gap-1">
+          <span>Learn more</span>
+          <ArrowRight className="h-5 w-5" />
+        </button>
       </div>
     </section>
   );
