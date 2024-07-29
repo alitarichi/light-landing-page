a96aa15faa710af861d62a60bdc32640484568a1
ali tarichi <tarichiali@gmail.com>
2024-07-29 18:12:37 +0300

    cta inital commit

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,17 @@
 export const CallToAction = () => {
-  return "callToAction";
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+      <div className="container">
+        <h2 className="section-title">Sign up for free today</h2>
+        <p className="section-description">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts .
+        </p>
+      </div>
+      <div>
+        <button>Get for free</button>
+        <button>Learn more</button>
+      </div>
+    </section>
+  );
 };
